e43a9b7dfea45c2e04c4d59a8f1eec953cef5885
SHG86 <stfn.hijmering@gmail.com>
2014-11-10 16:11:18 +0100

    Changed library description.

--- Diagnostics/Diagnostics/Properties/AssemblyInfo.cs
+++ Diagnostics/Diagnostics/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Diagnostics")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contains libraries for activity logging.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Diagnostics")]
