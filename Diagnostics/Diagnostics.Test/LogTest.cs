1f48d5c45d81e51a9024efd5c23f95af4ebb18a6
SHG86 <stfn.hijmering@gmail.com>
2014-11-10 12:07:11 +0100

    Contains the Diagnostic class library.
    Diagnostics class library project with an unit testing project.

---
+++ Diagnostics/Diagnostics.Test/LogTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diagnostics.Test
+{
+    [TestClass]
+    public class LogTest
+    {
+        [TestMethod]
+        public void WriteError()
+        {
+            Log.Message(Log.GetCurrentMethod(), "An error occured during data retrieval.", MessageType.Error);
+        }
+
+        [TestMethod]
+        public void WriteInfo()
+        {
+            Log.Message(Log.GetCurrentMethod(), "Data retrieved.", MessageType.Information);
+        }
+
+        [TestMethod]
+        public void WriteWarning()
+        {
+            Log.Message(Log.GetCurrentMethod(), "Not all data has been retrieved.", MessageType.Warning);
+        }
+
+        [TestMethod]
+        public void WriteCustom()
+        {
+            Log.Message(Log.GetCurrentMethod(), "This is a custom message.", MessageType.Custom, "Custom title");
+        }
+
+        [TestMethod]
+        public void WriteErrorCustomDirectory()
+        {
+            Log.Message(Log.GetCurrentMethod(), "An error occured during data retrieval.", MessageType.Error, string.Empty, @"c:\temp\customLogfile.txt");
+        }
+
+        [TestMethod]
+        public void WriteInfoCustomDirectory()
+        {
+            Log.Message(Log.GetCurrentMethod(), "Data retrieved.", MessageType.Information, string.Empty, @"c:\temp\customLogfile.txt");
+        }
+
+        [TestMethod]
+        public void WriteWarningCustomDirectory()
+        {
+            Log.Message(Log.GetCurrentMethod(), "Not all data has been retrieved.", MessageType.Warning, string.Empty, @"c:\temp\customLogfile.txt");
+        }
+
+        [TestMethod]
+        public void WriteCustomCustomDirectory()
+        {
+            Log.Message(Log.GetCurrentMethod(), "This is a custom message.", MessageType.Custom, "Custom title", @"c:\temp\customLogfile.txt");
+        }
+
+    }
+}
---
+++ Diagnostics/Diagnostics/Log.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Diagnostics
+{
+    public enum MessageType { Error, Warning, Information, Custom }
+
+    public static class Log
+    {
+        //INFO: Application information.
+        //ERROR: Something went wrong and the application can't continue anymore.
+        //WARNING: Something went wrong but the application can still continue.
+        public static void Message(string source, string message, MessageType type, string customTitle = "", string filePath = @"c:\temp\log.txt" )
+        {
+            //TODO: the parameters should be send through a single object instead of loose parameters.
+            //TODO: build exception handlers (eg. when the program can't access the directory/file).
+            //TODO: filePath should be dynamic.
+
+            var sb = new StringBuilder();
+            sb.Append("[ApplicationName]"); //TOOD: make the ApplicationName dynamic.
+            sb.Append(string.Format("[{0}] ", DateTime.Now));
+
+            switch (type)
+            {
+                case MessageType.Information:
+
+                    sb.Append("[INFO] ");
+                    break;
+
+                case MessageType.Warning:
+
+                    sb.Append("[WARNING] ");
+                    break;
+
+                case MessageType.Error:
+
+                    sb.Append("[ERROR] ");
+                    break;
+
+                case MessageType.Custom:
+
+                    customTitle = customTitle.ToUpper();
+                    sb.Append(string.Format("[{0}] ", customTitle));
+                    break;
+
+                default:
+
+                    sb.Append("[UNKNOWN] ");
+                    break;
+            }
+            sb.Append(string.Format("[{0}] ", source));
+            sb.Append(message);
+            Console.WriteLine(sb.ToString());
+            _WriteToFile(sb.ToString(), filePath);
+        }
+
+        private static void _WriteToFile(string message, string path)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(message);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static string GetCurrentMethod()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
+
+            return sf.GetMethod().Name;
+        }
+
+    }
+}
